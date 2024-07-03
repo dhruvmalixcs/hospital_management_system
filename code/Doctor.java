1f69b200676e72f46d3e7cb99cdb82166eed6c12
DHRUV MALIK <90127439+dhruvmalixcs@users.noreply.github.com>
2024-07-04 04:21:06 +0530

    Update Doctor.java

--- code/Doctor.java
+++ code/Doctor.java
@@ -7,46 +7,52 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Doctor {
+
     private Connection connection;
 
+
     public Doctor(Connection connection){
-        this.connection = connection;
+        this.connection=connection;
     }
 
     public void viewDoctors(){
         String query = "select * from doctors";
         try{
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            //resultSet helps in assigning the data to a variable and moving the pointer
             ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println("Doctors: ");
-            System.out.println("+------------+--------------------+------------------+");
-            System.out.println("| Doctor Id  | Name               | Specialization   |");
-            System.out.println("+------------+--------------------+------------------+");
+
+
+            System.out.println("Doctors");
+            System.out.println("+------------+---------------------+-----------------------------+");
+            System.out.println("| Doctor ID  | name                | specialization              |");
+            System.out.println("+------------+---------------------+-----------------------------+");
+
+
             while(resultSet.next()){
                 int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
+                String name=resultSet.getString("name");
                 String specialization = resultSet.getString("specialization");
-                System.out.printf("| %-10s | %-18s | %-16s |\n", id, name, specialization);
-                System.out.println("+------------+--------------------+------------------+");
-            }
+                System.out.printf("|%-12s|%-21s|%-29s|\n",id ,name,specialization);
 
-        }catch (SQLException e){
+
+            }
+        }
+        catch (SQLException e){
             e.printStackTrace();
         }
     }
-
-    public boolean getDoctorById(int id){
-        String query = "SELECT * FROM doctors WHERE id = ?";
+    public boolean checkDoctorsByID(int id){
+        String query="Select * FROM doctors where id = ?";
         try{
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                return true;
-            }else{
-                return false;
-            }
-        }catch (SQLException e){
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()) return true;
+            else return false;
+
+        }
+        catch (SQLException e){
             e.printStackTrace();
         }
         return false;
